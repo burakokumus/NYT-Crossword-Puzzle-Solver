6ef360caf33689c21b29ccd7db47ac2b8e8577a5
Yüce <yuceyuce99@gmail.com>
2020-12-13 21:22:12 +0300

    Implement antonyms synonyms and definitions

--- merriam.py
+++ merriam.py
@@ -1,27 +1,64 @@
+from time import process_time
 import requests
+import json
 
 DICTIONARY_KEY = "340a0202-dd9f-461a-857b-7a401ca334e8"
 THESAURUS_KEY  = "784474a4-5968-45da-8f98-d987d2c4935d"
 
+def get_ants_and_syns(word):
+    thes_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" + word + "?key=" + THESAURUS_KEY
+    response = requests.get(thes_url)
+    r = response.json()
+    synonyms = []
+    antonyms = []
+    for result in r:
+        for fields in result["meta"].items():
+            # If result is not the exact match of the word, skip
+            if fields[0] == "id":
+                if fields[1] != word:
+                    break
+            # Add synonyms
+            if fields[0] == "syns":
+                for syn_list in fields[1]:
+                    for syn in syn_list: 
+                        synonyms.append(syn)
+            # Add antonyms
+            if fields[0] == "ants":
+                for ant_list in fields[1]:
+                    for ant in ant_list: 
+                        antonyms.append(ant)
+    return antonyms, synonyms
+
 def get_dictionary_result(word):
     dict_url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/" + word + "?key=" + DICTIONARY_KEY
-    r = requests.get(dict_url)
-    # process the .json file
-    return r.json()
-
-def get_thesaurus_result(word):
-    thes_url = "https://www.dictionaryapi.com/api/v3/references/thesaurus/json/" + word + "?key=" + THESAURUS_KEY
-    r = requests.get(thes_url)
-    # process the .json file
-    return r.json()
+    response = requests.get(dict_url)
+    r = response.json()
+    definitions = []
+    related = False
+    for result in r:
+        related = False
+        for meta_items in result["meta"].items():
+            # If result is not the exact match of the word, skip
+            if meta_items[0] == "id":
+                if meta_items[1] == word:
+                    related = True
+                    break
+        if related:
+            # Add each definition
+            for definition in result["shortdef"]:
+                definitions.append(definition)
+    return definitions
 
 if __name__ == '__main__':
     word = input("Enter a word\n").lower()
-    choice = int(input("Which one do you need?\n1. Dictionary\n2. Thesaurus\n"))
+    choice = int(input("Which one do you need?\n1. Dictionary\n2. Antonyms\n3. Synonyms\n"))
     if choice == 1:
         print(get_dictionary_result(word))
-    elif choice == 2:
-        print(get_thesaurus_result(word))
+    elif choice == 2 or choice == 3:
+        ants, syns = get_ants_and_syns(word)
+        if choice == 2:
+            print(ants)
+        elif choice == 3:
+            print(syns)
     else:
         print("Wrong input")
-    
