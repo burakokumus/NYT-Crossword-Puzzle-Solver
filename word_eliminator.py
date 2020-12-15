6dd632c201446b7b3be91b603dc5d737adeb4f34
burakokumus <burakokumus98@gmail.com>
2020-12-15 17:52:36 +0300

    Add filter_words method to word_eliminator

--- word_eliminator.py
+++ word_eliminator.py
@@ -11,8 +11,8 @@ def eliminate_duplicates(word_list):
 def eliminate_punctuation(word_list):
     return [s.translate(str.maketrans('', '', string.punctuation)) for s in word_list]
 
-def eliminate_long_words(word_list):
-    return [word for word in word_list if len(word) <= 5 and len(word) > 1] 
+def eliminate_long_or_short_words(word_list, word_length):
+    return [word for word in word_list if len(word) == word_length] 
 
 def eliminate_numbers(word_list):
     return [word for word in word_list if not has_numbers(word)]
@@ -22,3 +22,13 @@ def eliminate_stop_words(word_list):
 
 def to_upper_case(word_list):
     return [x.upper() for x in word_list]
+
+def filter_words(word_list, word_length):
+    unique_words = eliminate_duplicates(word_list)
+    words_without_punc = eliminate_punctuation(unique_words)
+    words_with_correct_length = eliminate_long_or_short_words(words_without_punc)
+    words_without_number = eliminate_numbers(words_with_correct_length)
+    words_without_stop_words = eliminate_stop_words(words_without_number)
+    words_with_lower_case = to_upper_case(words_without_stop_words)
+    
+    return words_with_lower_case
