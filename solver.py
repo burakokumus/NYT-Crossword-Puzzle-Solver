9706bc77e44dc1b4c10fbbc27f770b131338905e
Yüce <yuceyuce99@gmail.com>
2020-12-15 15:16:55 +0300

    Implement finding the start end the end of the space for answers

--- solver.py
+++ solver.py
@@ -1,6 +1,35 @@
 import string
 from nltk.corpus import stopwords
 
+SOLUTION = {
+    "date": "Thursday, November 5, 2020", 
+    "grid": [   [1, 1, 0, 0, 0], 
+                [0, 0, 0, 0, 0], 
+                [0, 0, 0, 0, 0], 
+                [0, 0, 0, 0, 0], 
+                [0, 0, 0, 1, 1] ], 
+    "grid_numbers": [   [0, 0, "1", "2", "3"], 
+                        ["4", "5", 0, 0, 0], 
+                        ["6", 0, 0, 0, 0], 
+                        ["7", 0, 0, 0, 0], 
+                        ["8", 0, 0, 0, 0]  ], 
+    "answer": [ [" ", " ", "T", "A", "B"], 
+                ["S", "M", "I", "L", "E"], 
+                ["K", "A", "P", "P", "A"], 
+                ["I", "T", "S", "O", "N"], 
+                ["S", "H", "Y", " ", " "]], 
+    "across": [ ["1", "Running total at a bar"], 
+                ["4", "Photographer's request"], 
+                ["6", "Greek \"K\""], 
+                ["7", "\"Oh, you wanna go? Let's go!\""], 
+                ["8", "Bashful"]], 
+    "down": [   ["1", "A little drunk"], 
+                ["2", "Purina dog food brand"], 
+                ["3", "Word after jelly or coffee"], 
+                ["4", "Sports equipment with which you can do a \"pizza stop\""], 
+                ["5", "Class that has its pluses and minuses"]]
+}
+
 def solve(  grid, across, down, grid_numbers):
     answer = [
                 [' ', ' ', ' ', ' ', ' '], 
@@ -9,7 +38,34 @@ def solve(  grid, across, down, grid_numbers):
                 [' ', ' ', ' ', ' ', ' '], 
                 [' ', ' ', ' ', ' ', ' '], 
     ]
+    clue = "Bashful"
+    clue_way = "down"
+    clue_no = 2
+    print(start_and_end(grid, grid_numbers, clue_no, clue_way))
+
     return answer
+
+def start_and_end(grid, grid_numbers, clue_no, direction):
+    starting_pos = start_position(grid_numbers, clue_no)
+    ending_pos = end_position(grid, starting_pos, direction) 
+    return starting_pos, ending_pos
+
+def start_position(grid_numbers, clue_no):
+    for x in range(5):
+        for y in range(5):
+            if int(grid_numbers[x][y]) == clue_no:
+                return (x, y)
+
+def end_position(grid, start_position, direction):
+    x = start_position[0]
+    y = start_position[1]
+    if direction == "across":
+        while y < 5 and grid[x][y + 1] != 1:
+            y += 1
+    elif direction == "down":
+        while x < 5 and grid[x + 1][y] != 1:
+            x += 1
+    return (x, y)    
 
 def has_numbers(inputString):
     return any(char.isdigit() for char in inputString)
@@ -25,3 +81,5 @@ def process_list(input_list):
     none_stop_words = [x for x in none_number_words if x not in stopwords.words('english')] # remove stop-words
     result_words = [x.lower() for x in none_stop_words]
     return result_words
+
+solve(SOLUTION["grid"], SOLUTION["across"], SOLUTION["down"], SOLUTION["grid_numbers"])
